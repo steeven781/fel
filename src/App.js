0a64b4d8aa4da7eb285d5f2c68e6e68ed93dc0ab
Emilio Gonzalez <emiliofgonzalez7@gmail.com>
2021-09-23 20:41:16 -0600

    actualizacion de importaciones mui v5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Routes from './Routes';
 
@@ -11,15 +11,15 @@ function App() {
 
   const theme = createTheme(
     {
-      palette: { 
-        mode: 'dark', 
+      palette: {
+        mode: 'dark',
         common: {
           black: "#000",
-          white:"#fff"
+          white: "#fff"
         },
-        primary:{
-          main:"#90caf9",
-          light:"#e3f2fd",
+        primary: {
+          main: "#90caf9",
+          light: "#e3f2fd",
           dark: "#42a5f5",
           contrastText: "#fff"
         }
@@ -28,14 +28,14 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-       <CssBaseline/>
-        <div className="App">
+      <CssBaseline />
+      <div className="App">
         <BrowserRouter>
-                <Routes/>
-        </BrowserRouter> 
-        </div>
+          <Routes />
+        </BrowserRouter>
+      </div>
     </ThemeProvider>
-    
+
   );
 }
 
